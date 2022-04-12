c3ccaf9c5a19ac262d04623df85ebac3afe59940
Oka Sigit <oka.sigit9@gmail.com>
2022-04-12 14:32:14 +0700

    udpate link local css

--- application/views/admin/flow_4.php
+++ application/views/admin/flow_4.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?= base_url('node_modules/drawflow/dist/bootstrap.min.css') ?>" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 
     <!-- drawflow -->
     <link rel="stylesheet" href="<?= base_url('node_modules/drawflow/dist/drawflow.css') ?>">
